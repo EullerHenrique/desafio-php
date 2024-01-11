d0ded4caf46e7a43651ede7aca67b88f11c13af9
Euller Henrique Bandeira Oliveira <euller.oliveira@arphoenix.com.br>
2024-01-11 16:50:55 -0300

    feat

--- web/pages/crud/read/BuscarEnderecoContato.php
+++ web/pages/crud/read/BuscarEnderecoContato.php
@@ -3,6 +3,12 @@
 include('../inc/conecta.php');
 
 $idContato = $_GET['id'];
+if(!$idContato){
+    die('Erro! ID não informado');
+}else if(!is_numeric($idContato)){
+    die('Erro! ID inválido');
+}
+
 $sql = "SELECT * FROM ENDERECO WHERE ID_CONTATO = $idContato";
 $queryEndereco = mysql_query($sql, $link);
 if (!$queryEndereco){
