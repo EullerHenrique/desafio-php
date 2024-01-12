8fc2f1fd222505a4e3937c1c0d5a0bf47bc2ea13
Euller Henrique Bandeira Oliveira <euller.oliveira@arphoenix.com.br>
2024-01-11 22:03:04 -0300

    feat:Paginação

--- web/pages/crud/read/ListarContatos.php
+++ web/pages/crud/read/ListarContatos.php
@@ -2,10 +2,33 @@
 
 include('../inc/conecta.php');
 
-$sql = "SELECT * FROM CONTATO ORDER BY NOME ASC";
-$queryContatos = mysql_query($sql, $link);
-if (!$queryContatos){
-    die('Database error: ' . mysql_error());
+$totalPorPagina = $_GET['perPage'];
+if(!$totalPorPagina){
+    $totalPorPagina = 5;
 }
+
+$sqlTotal = "SELECT COUNT(*) as total FROM CONTATO";
+$queryTotal = mysql_query($sqlTotal, $link);
+$totalLinhas = mysql_result($queryTotal, 0, 'total');
+$totalPaginas = ceil($totalLinhas / $totalPorPagina);
+
+$paginaAtual = $_GET['page'];
+if (!$paginaAtual || $paginaAtual < 0 || !is_numeric($paginaAtual) || $paginaAtual == 0 || $paginaAtual > $totalPaginas) {
+    $paginaAtual=1; 
+} 
+
+//Ex: 0 * 10 = 0
+//Ex: 1 * 10 = 10
+$indiceInicial = ($paginaAtual-1) * $totalPorPagina;
+
+//LIMIT -> Primeiro parametro é o índice do ínicio e o segundo é o total de registros
+$sqlLimit = "SELECT * FROM CONTATO ORDER BY NOME ASC LIMIT $indiceInicial, $totalPorPagina";
+$queryContatos = mysql_query($sqlLimit, $link);
+if (!$queryContatos){
+        die('Database error: ' . mysql_error());
+}   
+
+$paginaAnterior = $paginaAtual==1?1:$paginaAtual-1;
+$paginaPosterior = $paginaAtual>$totalPaginas?$paginaAtual:$paginaAtual+1;
 
 ?>
--- web/pages/index.php
+++ web/pages/index.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 
@@ -9,39 +10,105 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- JQUERY -->
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
-    
+    <!-- PHP -->
+    <?php include('crud/read/ListarContatos.php'); ?>
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
 </head>
 
 <body>
 
-    <div class="container">
-        <h1 style="text-align: center;">Contatos</h1>
-        
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>Nome</th>
-                    <th>Email</th>
-                    <th>Telefone</th>
-                    <th>Endereço</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php include('crud/read/ListarContatos.php'); ?>
-                <?php while ($contato = mysql_fetch_assoc($queryContatos)) { ?>
-                    <tr>
-                        <td><?php echo utf8_encode($contato['NOME']); ?></td>
-                        <td><?php echo utf8_encode($contato['EMAIL']); ?></td>
-                        <td><?php echo utf8_encode($contato['TELEFONE']); ?></td>
-                        <td><a href="endereco.php?id=<?php echo $contato['ID']; ?>">Visualizar</a></td>
-                    </tr>
-                <?php } ?>
-            </tbody>
-        </table>
-    </div>
+    <header>
+    
+        <div class="container">
+            <h1 style="text-align: center;">Contatos</h1>
+        </div>
+    </header>
 
-    <!-- BOOTSTRAP -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <main>
+        <div class="container">
+
+        <div class="row">
+
+            <div class="d-flex align-items-center mt-3 mb-3">
+                <span class="ms-4">Exibir</span>
+                <select class="form-select perPage ms-3 me-3" onChange="alterarPerPage()">
+                    <option value="5" <?php if ($_GET['perPage'] == 5) echo 'selected'; ?>>5</option>
+                    <option value="10" <?php if ($_GET['perPage'] == 10) echo 'selected'; ?>>10</option>
+                    <option value="15" <?php if ($_GET['perPage'] == 15) echo 'selected'; ?>>15</option>
+                    <option value="20" <?php if ($_GET['perPage'] == 20) echo 'selected'; ?>>20</option>
+                    <option value="25" <?php if ($_GET['perPage'] == 25) echo 'selected'; ?>>25</option>
+                    <option value="50" <?php if ($_GET['perPage'] == 50) echo 'selected'; ?>>50</option>
+                    <option value="100" <?php if ($_GET['perPage'] == 100) echo 'selected'; ?>>100</option>
+                </select>
+                <span>resultados por página</span>
+            </div>
+        
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Email</th>
+                        <th>Telefone</th>
+                        <th>Endereço</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php while ($contato = mysql_fetch_assoc($queryContatos)) { ?>
+                        <tr>
+                            <td><?php echo utf8_encode($contato['NOME']); ?></td>
+                            <td><?php echo utf8_encode($contato['EMAIL']); ?></td>
+                            <td><?php echo utf8_encode($contato['TELEFONE']); ?></td>
+                            <td><a href="endereco.php?id=<?php echo $contato['ID']; ?>">Visualizar</a></td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+
+             
+            <div class="d-flex justify-content-center">
+                <nav>                
+                    <ul class="pagination justify-content-center">
+                        <li class="page-item">
+                            <a class="page-link" href="index.php?page=1">
+                                    <span>&laquo;</span>
+                            </a>
+                        </li>
+                        <li class="page-item">
+                            <a class="page-link" href="index.php?page=<?php echo $paginaAnterior ?>">
+                                    <span>&lt;</span>
+                            </a>
+                        </li>
+                        <?php for ($page=1; $page <= $totalPaginas; $page++) { 
+                                $active = ($page == $paginaAtual) ? 'active' : '';
+                                echo "<li class='page-item $active'>
+                                        <a class='page-link' href='index.php?page=$page'>$page</a>
+                                     </li>";    
+                        } ?>
+                        <li class="page-item">
+                            <a class="page-link" href="index.php?page=<?php echo $paginaPosterior ?>">
+                                    <span>&gt;</span>
+                        </a>
+                        </li>
+                        <li class="page-item">
+                            <a class="page-link" href="index.php?page=<?php echo $totalPaginas ?>" >
+                                    <span >&raquo;</span>
+                            </a>
+                         </li>
+                    </ul>
+                </nav>
+            </div>
+
+    </main>
+
+<script>
+        function alterarPerPage() {
+            let perPage = $('.perPage').val();
+            window.location.href = 'index.php?page='+<?php echo $paginaAtual ?>+'&perPage=' + perPage;
+        }
+</script>    
+<!-- BOOTSTRAP -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
 </html>
